99e200554eac3fc52c074c0187390d2dcbf9a1c6
nivwer <nivwerr@gmail.com>
2022-11-15 12:04:14 -0300

    update

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -2,10 +2,10 @@ function palindrome(str) {
     var stringInput = str.replace(/[(-\/_,.) ]/g, "").toLowerCase();
     var reversedString = stringInput.split('').reverse().join('');
     return (stringInput === reversedString) ? "Is palindrome" : "Is not palindrome";
-  }
+}
 
 function checker(){
-  let text = document.getElementById("text").value;
-  text = palindrome(text);
-  document.getElementById("res").value = text;
+    let text = document.getElementById("text").value;
+    text = palindrome(text);
+    document.getElementById("res").value = text;
 }
